778890803a89d8c251835e4c7622e13ede09e4ca
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 15:59:07 -0500

    🚀 RELEASE: deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Sunat Catálogos',
-    description: 'SUNAT : Catálogo de Códigos',
+    description: 'Lista de catálogos utilizados por SUNAT para facturación electrónica.',
+    base: '/SunatCatalogos/',
     themeConfig: {
         searchPlaceholder: 'Buscar...',
         lastUpdated: 'Última Actualización',
@@ -12,15 +13,15 @@ module.exports = {
     },
     head: [
         ['meta', {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no"}],
-        ['link', { rel: 'icon', href: '/icon.svg' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#1e88e5' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png' }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#1e88e5' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#1e88e5' }]
+        ['link', {rel: 'icon', href: '/icons/favicon-32x32.png'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['meta', {name: 'theme-color', content: '#1e88e5'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        ['link', {rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png'}],
+        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#1e88e5'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/icons/mstile-144x144.png'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#1e88e5'}]
     ],
     plugins: {
         '@vuepress/pwa': {
