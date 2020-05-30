736cd4cacfb5d47542cf6ae62047319eac437dce
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 09:41:27 -0500

    📦 NEW: 58 - Código de tipo de medidor (recibo de luz)

---
+++ data/08/58.sql
@@ -0,0 +1,7 @@
+CREATE TABLE table_58(
+   codigo      VARCHAR(1) NOT NULL PRIMARY KEY
+  ,descripcion VARCHAR(10) NOT NULL
+);
+INSERT INTO table_58(codigo,descripcion) VALUES
+ ('1','Trifásico')
+,('2','Monofásico');
