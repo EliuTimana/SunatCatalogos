934c8a0266f22a6fe7265cc5888504a0759cfb19
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 09:39:54 -0500

    📦 NEW: 57 - Código de tipo de servicio públicos - telecomunicaciones

---
+++ data/08/57.sql
@@ -0,0 +1,9 @@
+CREATE TABLE table_57(
+   codigo      VARCHAR(1) NOT NULL PRIMARY KEY
+  ,descripcion VARCHAR(33) NOT NULL
+);
+INSERT INTO table_57(codigo,descripcion) VALUES
+ ('1','Servicios Portadores')
+,('2','Teleservicios o Servicios Finales')
+,('3','Servicios de Difusión')
+,('4','Servicios de valor añadido');
