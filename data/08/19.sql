16493d7d7f3098ae4746749009cf60cd46fd1d6b
Eliú Timaná <EliuTimana@gmail.com>
2020-05-29 22:30:00 -0500

    📦 NEW: 19 - estado del ítem (resumen diario)

---
+++ data/08/19.sql
@@ -0,0 +1,9 @@
+CREATE TABLE table_19
+(
+    codigo      VARCHAR(1) NOT NULL PRIMARY KEY,
+    descripcion VARCHAR(9) NOT NULL
+);
+INSERT INTO table_19(codigo, descripcion)
+VALUES ('1', 'Adicionar')
+     , ('2', 'Modificar')
+     , ('3', 'Anulado');
