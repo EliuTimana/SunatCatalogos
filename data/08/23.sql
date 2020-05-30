d1382b29747ec6525e3a8da6a00f4564af6e5cba
Eliú Timaná <EliuTimana@gmail.com>
2020-05-29 22:40:21 -0500

    📦 NEW: 23 - regimen de retenciones

---
+++ data/08/23.sql
@@ -0,0 +1,8 @@
+CREATE TABLE table_23
+(
+    codigo      VARCHAR(2) NOT NULL PRIMARY KEY,
+    descripcion DATE       NOT NULL
+);
+INSERT INTO table_23(codigo, descripcion)
+VALUES ('01', 'Tasa 3%')
+     , ('02', 'Tasa 6%');
