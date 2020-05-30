ba79836460712213e6ac26c89b686dcfe02d4ba2
Eliú Timaná <EliuTimana@gmail.com>
2020-05-29 22:27:35 -0500

    📦 NEW: 18 - modalidad de transporte

---
+++ data/08/18.sql
@@ -0,0 +1,8 @@
+CREATE TABLE table_18
+(
+    codigo      VARCHAR(2)  NOT NULL PRIMARY KEY,
+    descripcion VARCHAR(18) NOT NULL
+);
+INSERT INTO table_18(codigo, descripcion)
+VALUES ('01', 'Transporte público')
+     , ('02', 'Transporte privado');
