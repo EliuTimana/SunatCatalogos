642367ca78ecad1d51deeb4d8ab6aec75f7f8038
Eliú Timaná <EliuTimana@gmail.com>
2020-05-29 21:43:05 -0500

    📦 NEW: 06 - Código de tipo de documento de identidad

---
+++ data/08/06.sql
@@ -0,0 +1,15 @@
+CREATE TABLE table_06(
+   codigo      VARCHAR(1) NOT NULL PRIMARY KEY
+  ,descripcion VARCHAR(48) NOT NULL
+);
+INSERT INTO table_06(codigo,descripcion) VALUES
+ ('0','DOC.TRIB.NO.DOM.SIN.RUC')
+,('1','Documento Nacional de Identidad')
+,('4','Carnet de extranjería')
+,('6','Registro Unico de Contributentes')
+,('7','Pasaporte')
+,('A','Cédula Diplomática de identidad')
+,('B','DOC.IDENT.PAIS.RESIDENCIA-NO.D')
+,('C','Tax Identification Number - TIN – Doc Trib PP.NN')
+,('D','Identification Number - IN – Doc Trib PP. JJ')
+,('E','TAM- Tarjeta Andina de Migración');
