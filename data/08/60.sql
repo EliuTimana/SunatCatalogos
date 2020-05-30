0151fdc7d93f660902dfe81cbab76a7a5a4c6f1a
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 09:44:57 -0500

    📦 NEW: 60 -  Código de tipo de dirección

---
+++ data/08/60.sql
@@ -0,0 +1,10 @@
+CREATE TABLE table_60(
+   codigo      VARCHAR(2) NOT NULL PRIMARY KEY
+  ,descripcion VARCHAR(14) NOT NULL
+);
+INSERT INTO table_60(codigo,descripcion) VALUES
+ ('01','Punto de venta')
+,('02','Producción')
+,('03','Extracción')
+,('04','Explotación')
+,('05','Otros');
