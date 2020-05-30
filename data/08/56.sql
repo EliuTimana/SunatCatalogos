93a84d177dad5d136a24178ae2d1ca4ba6bc910d
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 09:38:22 -0500

    📦 NEW: 56 - Código de tipo de servicio público

---
+++ data/08/56.sql
@@ -0,0 +1,12 @@
+CREATE TABLE table_56(
+   codigo      VARCHAR(1) NOT NULL PRIMARY KEY
+  ,descripcion VARCHAR(37) NOT NULL
+);
+INSERT INTO table_56(codigo,descripcion) VALUES
+ ('1','Energía eléctrica')
+,('2','Agua')
+,('3','Cable')
+,('4','Internet')
+,('5','Otros servicios regulados por OSIPTEL')
+,('6','Gas natural')
+,('7','Telefonía');
