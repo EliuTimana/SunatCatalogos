4ac4b914ab956eb703ac7390ddf82069e25dcabc
Eliú Timaná <EliuTimana@gmail.com>
2020-05-30 15:14:52 -0500

    📦 NEW: pwa resources

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,16 @@ module.exports = {
         ]
     },
     head: [
-        ['meta', {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no"}]
+        ['meta', {name: "viewport", content: "width=device-width, initial-scale=1, shrink-to-fit=no"}],
+        ['link', { rel: 'icon', href: '/icon.svg' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#1e88e5' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#1e88e5' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#1e88e5' }]
     ],
     plugins: {
         '@vuepress/pwa': {
